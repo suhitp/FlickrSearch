f52c9190753e36e0b670f62dd750352eebaac1f7
Suhit Patil <suhit.patil@upgrad.com>
2017-06-04 12:28:49 +0530

    Created VIPER Module for FlickrSearch along with Module Builder class

---
+++ FlickrSearch/Modules/FlickrSearch/Interactor/FlickrSearchInteractor.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrSearchInteractor.swift
+//  FlickrSearch
+//
+//  Created by Suhit Patil on 04/06/17.
+//  Copyright © 2017 Suhit Patil. All rights reserved.
+//
+
+import UIKit
+
+class FlickrSearchInteractor: NSObject {
+
+}
---
+++ FlickrSearch/Modules/FlickrSearch/View/FlickrSearchViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrSearchViewController.swift
+//  FlickrSearch
+//
+//  Created by Suhit Patil on 04/06/17.
+//  Copyright © 2017 Suhit Patil. All rights reserved.
+//
+
+import UIKit
+
+class FlickrSearchViewController: UIViewController {
+
+}
